872ed1081a7b2685c1e918d8aa9a7416e6167387
GreysonGough <greygough@gmail.com>
2026-01-29 09:25:36 -0800

    a400

--- task1.py
+++ task1.py
@@ -8,8 +8,28 @@ Ask the user to enter a letter from A to C
 Execute the function of the letter they use.
 """
 
-def A():
 
+
+def A():
+    print("Hello")
 def B():
+    print("How are you")
 
 def C():
+    print("Hi")
+
+def main():
+    functions_map = {"A":A, "B":B, "C":C}
+    user_input = input("choose 'A', 'B', or 'C' ")
+
+    if user_input in functions_map:
+        
+        func_to_call = functions_map[user_input]
+            
+        func_to_call()
+    else:
+        print("Invalid choice. Please enter A, B, or C.")
+
+if __name__ == "__main__":
+    main()
+
--- task2.py
+++ task2.py
@@ -10,3 +10,47 @@ plays the game
 This will be silimar to something you have already done, but in this task you 
 are breaking the code up into different sections to make each a function.
 """
+import random
+import time
+
+def title():
+    print("Welcome to the guessing game!")
+    time.sleep(1)
+    print("This game involves you trying to guessing the number in few tries as possible.")
+    time.sleep(1.4)
+    print("The number is from 1 - 100")
+    time.sleep(1)
+    print("Good luck!")
+
+def game():
+    rnum = random.randint(1, 100)
+    guess_count = 0
+
+    while True:
+        try:
+            guess_count += 1
+            guess = int(input(f"Enter your guess:"))
+
+            if guess < 1 or guess > 100:
+                print("Please guess within 1 - 100.")
+
+            elif guess == rnum:
+                print(f"congrats you won. {guess_count} guesses.")
+                break
+
+            elif guess < rnum:
+                print("Too low! Please try again")
+
+            elif guess > rnum:
+                print("Too high! Please try again")
+
+        except ValueError:
+            print("Please enter a valid value.")
+            guess_count += 1
+         
+        
+if __name__ == "__main__":
+    title()
+    game()
+
+
